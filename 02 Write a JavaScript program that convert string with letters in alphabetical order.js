eaacf91c997901b92dc9420c90704c0a336d0752
Abdul Wahab Shaikh <aw599822@gmail.com>
2023-12-05 11:55:52 +0500

    02 file

---
+++ 02 Write a JavaScript program that convert string with letters in alphabetical order.js
@@ -0,0 +1,8 @@
+let Name= "Abdulwahabshaikh";
+let toarray = Name.split(""); console.log(toarray)
+
+let sortarrayofstring= toarray.sort();
+console.log(sortarrayofstring) // printing array sorted
+let convertarrayintostring =  sortarrayofstring.join("");
+console.log(convertarrayintostring);
+
