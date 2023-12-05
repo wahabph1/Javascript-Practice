e85e0ea02f7ce91d535c7f50cbecb3e3727982ca
Abdul Wahab Shaikh <aw599822@gmail.com>
2023-12-05 14:01:32 +0500

    05 file

---
+++ 05 Create a function that filters an array of strings and includes only those with a length less than or.js
@@ -0,0 +1,10 @@
+let arrayofstrings = ["Abdul","wahab","shaikh","21sw07","from","hala","new"]
+
+arrayofstrings =arrayofstrings.filter((name)=> name.length<5);
+console.log(arrayofstrings)
+// to print every string with for of loop
+for (const name of arrayofstrings) {
+    console.log(name)
+    
+}
+
