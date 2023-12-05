0f66830b2fa216dd7dd64302ac55e4d886995a1b
Abdul Wahab Shaikh <aw599822@gmail.com>
2023-12-05 12:21:03 +0500

    03 file

---
+++ 03 Write a JavaScrip program counts the number of vowels within the string.js
@@ -0,0 +1,3 @@
+let Names= "abdul wahab shaiaaaaaaaaaaaaaakh";
+let vowels = ['a','e','i','o','u']
+let toconvertinthearray = Names.split('').filter(char => vowels.includes(char)).length;console.log(toconvertinthearray)
