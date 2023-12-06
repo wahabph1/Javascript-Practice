f47608ac92d9c6088685a6aa409c90cf5041cf4d
Abdul Wahab Shaikh <aw599822@gmail.com>
2023-12-06 18:37:09 +0500

    15 file

---
+++ 15 User Write a program to flatten a nested array using the reduce function. For example, [1, [2, 3], [4, 5]] should become [1, 2, 3, 4, 5]..js
@@ -0,0 +1,8 @@
+let  Array =  [1, [2, 3], [4, 5]];
+Array = Array.reduce((accumaltor, ele) => {
+    console.log(`acumaltor is = ${accumaltor} and cuurent element is = ${ele}`)
+    return accumaltor.concat(ele)
+  
+    
+}, [])
+console.log(Array)
