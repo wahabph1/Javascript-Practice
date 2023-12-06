ce3d2bf97b24ebea182fcb30b0036908aae90ada
Abdul Wahab Shaikh <aw599822@gmail.com>
2023-12-06 18:21:07 +0500

    after some changes

--- 13 Write a function to calculate the sum of all elements in an array using the reduce function..js
+++ 13 Write a function to calculate the sum of all elements in an array using the reduce function..js
@@ -1,7 +1,7 @@
 let numbers = [1,2,3,4,5,6,7];
 
 numbers = numbers.reduce((accumaltor,element)=>{
-    console.log(``)
+    console.log(`acumaltor is = ${accumaltor} and cuurent element is = ${element} and sum of them is = ${accumaltor+element} `)
     return accumaltor+element;
 },0)
 console.log(numbers)
