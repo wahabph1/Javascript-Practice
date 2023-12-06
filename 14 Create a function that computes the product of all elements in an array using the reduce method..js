13bbddfe26299ceac50cc25c96168a3dd68aea0d
Abdul Wahab Shaikh <aw599822@gmail.com>
2023-12-06 18:27:20 +0500

    14 file

---
+++ 14 Create a function that computes the product of all elements in an array using the reduce method..js
@@ -0,0 +1,9 @@
+let  Numbers = [1,2,3,4,5,6,7,8,9,10]
+
+Numbers = Numbers.reduce((accumaltor,element)=>{
+    console.log(`acumaltor is = ${accumaltor} and cuurent element is = ${element} and product of them is = ${accumaltor*element} `)
+
+    return accumaltor*element
+
+},1)
+console.log(Numbers)
