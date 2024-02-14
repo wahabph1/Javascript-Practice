c4be3d82275ffe1bee47735bd11b72ae27ee09f6
Abdul Wahab Shaikh <aw599822@gmail.com>
2024-02-14 21:08:54 +0500

    new file added

---
+++ 19 How to check is number is integer.js
@@ -0,0 +1,16 @@
+
+
+    var num = 10;
+    if (num%1===0)
+    {
+        console.log('it is interger')
+    }
+    else {
+        console.log("it is not integer")
+    }
+
+       // Different way is
+
+    console.log(
+        Number.isInteger(num))
+    
