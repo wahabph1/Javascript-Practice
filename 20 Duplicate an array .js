5cbc9bc6c7605409b5191b9a60b28a8d1ff76aa7
Abdul Wahab Shaikh <aw599822@gmail.com>
2024-02-14 21:41:10 +0500

    added new files

---
+++ 20 Duplicate an array .js
@@ -0,0 +1,5 @@
+function Duplicate(arr){
+    return arr.concat(arr)
+
+}
+console.log(Duplicate([1,2,3,4,5,6]))
---
+++ 21 reverse a number .js
@@ -0,0 +1,2 @@
+var a = 1222;
+console.log(a.toString().split('').reverse().join(""))
